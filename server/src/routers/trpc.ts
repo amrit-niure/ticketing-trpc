b95d2f627b08f6ff6e8cf47d2c4e7425321f43de
Amrit Niure <niureamrit2@gmail.com>
2025-05-27 13:09:03 +1000

    users trpc setup

--- server/src/routers/trpc.ts
+++
@@ -1,9 +0,0 @@
-// DEPRECATED: This file has been refactored into separate files for better organization
-// Please use the new organized structure:
-// - Context: ../trpc/context.ts
-// - Middleware: ../trpc/middleware.ts
-// - Procedures: ../trpc/procedures.ts
-// - Main exports: ../trpc/index.ts
-
-// Re-export everything from the new organized structure
-export * from '../trpc';
